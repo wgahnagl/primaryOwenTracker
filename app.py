01b16e4455f43f6b1bc96809d789296a5d1b4bea
skyler <puffballinthedark@gmail.com>
2017-11-10 12:48:10 -0500

    I don't understand how things work, and I just throw things at the wall to see what sticks.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder = '../primary_owen_tracker/primary_owen_tr
 
 @app.route("/")
 def main():
-    return "wht"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
