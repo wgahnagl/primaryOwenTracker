8d4b1014e2ac6fee947f9fe438d7e2e12a16ad0b
skyler <puffballinthedark@gmail.com>
2017-11-13 14:51:08 -0500

    trying to get LDAP working, but only a little bit.

--- primaryOwenTracker/__init__.py
+++ primaryOwenTracker/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from csh_ldap import CSHLDAP
+
 
 app = Flask(__name__)
+
+ldap = CSHLDAP(app.config["LDAP_BIND_DN"],
+app.config["LDAP_BIND_PW"])
+
+from primaryOwenTracker.ldap import ldap_is_eboard, ldap_is_rtp
 
 
 @app.route("/")
---
+++ primaryOwenTracker/ldap_owen_getter.py
@@ -0,0 +1,1 @@
+from CSHLDAP import CSHLDAP
