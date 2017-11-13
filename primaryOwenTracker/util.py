90537104fa0a1b70db012f58047c8827689ab8c6
skyler <puffballinthedark@gmail.com>
2017-11-13 15:28:32 -0500

    trying to auth still

---
+++ primaryOwenTracker/util.py
@@ -0,0 +1,16 @@
+from flask import session
+from functools import wraps
+
+
+def audiophiler_auth(func):
+    @wraps(func)
+    def wrapped_function(*args, **kwargs):
+        uuid = str(session["userinfo"].get("sub", ""))
+        uid = str(session["userinfo"].get("preferred_username", ""))
+        auth_dict = {
+            "uuid": uuid,
+            "uid": uid
+        }
+        kwargs["auth_dict"] = auth_dict
+        return func(*args, **kwargs)
+    return wrapped_function
