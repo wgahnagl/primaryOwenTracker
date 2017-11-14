0631a11a493e3416ddf7920dde6741b4e3202b07
skyler <puffballinthedark@gmail.com>
2017-11-14 00:50:21 -0500

    made a sumbission page

--- primaryOwenTracker/__init__.py
+++ primaryOwenTracker/__init__.py
@@ -4,11 +4,13 @@ import csh_ldap
 app = Flask(__name__)
 
 
-
-
 @app.route("/")
 def main():
    return render_template('index.html')
+
+@app.route("/submission")
+def submission():
+   return render_template('submission.html')
 
 if __name__ == "__main__":
     app.run()
