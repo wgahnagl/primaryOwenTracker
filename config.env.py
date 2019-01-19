da23939383cd548616ac646ec7ca67b3062aa50c
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-01-18 23:16:48 -0500

    the primary owen is back, baby

--- config.env.py
+++ config.env.py
@@ -1,3 +1,12 @@
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "moderated_owens.db")))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+OIDC_ISSUER = os.environ.get('OIDC_ISSUER', 'https://sso.csh.rit.edu/realms/csh')
+OIDC_CLIENT_CONFIG = {
+    'client_id': os.environ.get('POT_OIDC_CLIENT_ID', 'gallery'),
+    'client_secret': os.environ.get('POT_OIDC_CLIENT_SECRET', ''),
+    'post_logout_redirect_uris': [os.environ.get('GALLERY_OIDC_LOGOUT_REDIRECT_URI',
+                                                 'https://pot.csh.rit.edu/logout')]
+}
